fef88aa285dcfdfcccaf294e7da11052b6f42445
jappan-s <63873951+jappan-s@users.noreply.github.com>
2022-11-07 00:25:13 +0530

    Add files via upload

---
+++ map func (2).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+num = [1, 2, 3, 4, 5, 6, 7]
+print("Sample list: ", num)
+triple = map(lambda x: x*3, num) 
+print("Triple of list numbers:")
+print(list(triple))
+
+
+# In[ ]:
+
+
+
+
